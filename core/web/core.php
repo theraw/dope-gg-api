f3cb831e1083e0b9aa177bdf62923485ce43fc02
ƬHE ЯAW ☣ <daraw665@gmail.com>
2021-01-08 19:32:33 +0100

    Delete core.php

--- core/web/core.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require_once ('libs/MysqliDb.php');
-$mysqli = new mysqli ('127.0.0.1', 'dopegg', 'FhAtrm/jYFrEu81H454QjN4+', 'dopegg');
-$db = new MysqliDb ($mysqli);
