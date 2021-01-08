70a46cd95e8213abd159a514468ce4ba3cfd3c83
ƬHE ЯAW ☣ <daraw665@gmail.com>
2021-01-08 19:33:15 +0100

    Delete ioncube.sh

--- core/installers/ioncube.sh
+++
@@ -1,4 +0,0 @@
-cd /usr/local; wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
-tar xf ioncube_loaders_lin_x86-64.tar.gz; rm -Rf ioncube_loaders_lin_x86-64.tar.gz
-echo 'zend_extension = "/usr/local/ioncube/ioncube_loader_lin_7.2.so"' >> /etc/php/7.2/fpm/php.ini
-echo 'zend_extension = "/usr/local/ioncube/ioncube_loader_lin_7.2.so"' >> /etc/php/7.2/cli/php.ini
